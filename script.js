bcfbbfe5157b9b9dd2624a4fc725a2f3c76d032b
Gabrielle Skelton <gabrielleskeltonn@gmail.com>
2022-04-08 19:47:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -17,9 +17,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const database = getDatabase();
-const ref = database.ref();
-
-ref.on('value', snapshot => {
-  const data = snapshot.val();
-  console.log(data);
-});
